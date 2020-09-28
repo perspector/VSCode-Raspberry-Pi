b76534889932c92c8543b4377f44a6d86e4b9731
Benny <bennypythoncoder@gmail.com>
2020-09-27 20:08:43 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+!/bin/sh
+
+sudo apt-get install curl
+
+curl -s https://packagecloud.io/install/repositories/swift-arm/vscode/script.deb.sh | sudo bash
+sudo apt-get install code-oss
+
+sudo echo -e '# My aliases\nalias code="code-oss"' >> ~/.bashrc
+source ~/.bash_aliases
+clear
+echo 'The next time you open your terminal, you can launch Visual Studio code (code-oss) by typing "code" in your terminal, or by searching the menu for "Code-OSS"'
