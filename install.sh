39eb9129d3d6eb3881c2f6230d55879b256b1eff
Benny <bennypythoncoder@gmail.com>
2020-09-28 20:36:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
 echo "You may need to enter your password for the script to get superuser permissions so it can work. The password will not show."
+
+# Gets superuser privilages
 sudo -s
+
+# Gets the key
 wget -o - https://packagecloud.io/headmelted/codebuilds/gpgkey| sudo apt-key add -
+
+# Installs code-oss
 sudo apt-get install code-oss=1.29.0-1539702286
 apt-mark hold code-oss
