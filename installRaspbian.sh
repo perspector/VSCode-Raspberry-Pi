611fe88ceca639f7dca044771f3bb1d79e136fb3
Benny <bennypythoncoder@gmail.com>
2020-10-10 07:57:11 -0400

    Update installRaspbian.sh

--- installRaspbian.sh
+++ installRaspbian.sh
@@ -8,3 +8,5 @@ sudo wget -o - https://packagecloud.io/headmelted/codebuilds/gpgkey| sudo apt-ke
 sudo apt-get update
 sudo apt-get install code-oss=1.29.0-1539702286
 sudo apt-mark hold code-oss
+
+echo "Now reboot. The next time you open your terminal type code-oss or go to the applications menu and search for code-oss. This will launch the program."
