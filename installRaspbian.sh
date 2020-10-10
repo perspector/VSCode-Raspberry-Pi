76f2f015ce1d3dcfd3728240dd4afbc70c077a0c
Benny <bennypythoncoder@gmail.com>
2020-10-10 10:20:13 -0400

    Update installRaspbian.sh

--- installRaspbian.sh
+++ installRaspbian.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-echo "You may need to enter your password for the script to get superuser permissions so it can work. The password will not show."
 
-# Gets the key
-sudo wget -o - https://packagecloud.io/headmelted/codebuilds/gpgkey| sudo apt-key add -
-
-# Installs code-oss
-sudo apt-get update
-sudo apt-get install code-oss=1.29.0-1539702286
-sudo apt-mark hold code-oss
-
+sudo -s
+. <( wget -O - https://code.headmelted.com/installers/apt.sh )
 echo "Now reboot. The next time you open your terminal type code-oss or go to the applications menu and search for code-oss. This will launch the program."
