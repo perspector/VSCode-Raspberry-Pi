b3881d02f81bc10a599f54a67d130930cbd6290a
Benny <bennypythoncoder@gmail.com>
2020-11-25 02:25:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# sudo -s
+#sudo -s
+sudo -i
 . <( wget -O - https://code.headmelted.com/installers/apt.sh )
 echo "Now reboot. The next time you open your terminal type code-oss or go to the applications menu and search for code-oss. This will launch the program."
